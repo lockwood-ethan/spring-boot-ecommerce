97406c4039f237048182825a40a4f60d75b0c29e
ethanlockwood <lockwood.ethan@yahoo.com>
2025-06-08 12:35:52 -0400

    Getting category links working

--- src/main/java/com/megachubbzz/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/megachubbzz/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,14 @@
 package com.megachubbzz.ecommerce.dao;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.megachubbzz.ecommerce.entity.Product;
 
 @CrossOrigin(origins = "http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
